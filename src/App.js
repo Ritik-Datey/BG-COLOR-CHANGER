2f946055b463b21d12ea99ed1bcf19fbb403c5a9
Ritik vijay Datey <ritikdatey2017@gmail.com>
2024-07-10 12:06:18 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const handleButtonClick = (element) =>{
+    document.body.style.backgroundColor=element.target.value;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <button onClick={handleButtonClick} style={{backgroundColor:'green'}} value='green'>Green</button>
+      <button onClick={handleButtonClick} style={{backgroundColor:'red'}}  value='red'>Red</button>
+      <button onClick={handleButtonClick} style={{backgroundColor:'yellow'}}  value='yellow'>Yellow</button>
+      <button onClick={handleButtonClick} style={{backgroundColor:'orange'}}  value='orange'>Orange</button>
+      <button onClick={handleButtonClick} style={{backgroundColor:'purple'}}  value='purple'>Purple</button>
+      <button onClick={handleButtonClick} style={{backgroundColor:'gray'}}  value='gray'>Gray</button>
+      <button onClick={handleButtonClick} style={{backgroundColor:'black', color:'white'}}  value='black'>Black</button>
+      <button onClick={handleButtonClick} style={{backgroundColor:'pink'}}  value='pink'>Pink</button>
     </div>
   );
 }
